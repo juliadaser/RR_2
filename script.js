2e12177a6c7208c6cc94cb1ea77d4f9ce33f6c55
Julia Daser <98773977+juliadaser@users.noreply.github.com>
2023-09-05 13:52:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ rgb(255, 255, 255) = white
 
 COLOR INSPIRATION FROM : https://aifont.process.studio/
 */
-require('dotenv').config() 
-//// import '/dotenv/config'
+//require('dotenv').config() 
+import '/dotenv/config'
 
 colorSets = [
   ["rgb(234, 255, 0)", "rgb(0, 0, 0)"],
