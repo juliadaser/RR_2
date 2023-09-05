971030739bfb0248c98e36069c6dbebf7500b5af
Julia Daser <98773977+juliadaser@users.noreply.github.com>
2023-09-05 13:32:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ document.body.addEventListener("click", function () {
 function translateText() {
   const textToTranslate =
     "there are as many Internet Architectures as there are Users";
-  var apiKey = config.API_KEY;
+  var apiKey = process.env.API_KEY;
   const languages = [
     "fr",
     "es",
